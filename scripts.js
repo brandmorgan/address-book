932431613acf5183dcea7ad0268260f958e707fb
brandmorgan <brandmorgan@gmail.com>
2023-04-03 14:37:10 -0700

    start UI
    co-authored-by: Kyle Crawford <kcraw217@gmail.com>
    co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- scripts.js
+++ scripts.js
@@ -35,3 +35,13 @@ travelBook.addDestination(moscow);
 
 //User Interface logic
 
+function handleSelect(event) {
+  event.preventDefault();
+  const selection = document.getElementById("destination").value;
+  const pElement = document.createElement("p");
+  const p = document.querySelector("p");
+}
+
+window.addEventListener("load", function(){
+  document.getElementById("select-form").addEventListener("submit", handleSelect);
+});

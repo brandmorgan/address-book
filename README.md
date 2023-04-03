b86f7d57db595aaf413dc7575c331d6ae72e2854
brandmorgan <brandmorgan@gmail.com>
2023-04-03 13:34:13 -0700

    add imgs, addressBook methods, defined travelBook and destinations
    co-authored-by: Raed alkhanbashi <raed.alkhanbashi@gmail.com>
    co-authored-by: Kyle Crawford <kcraw217@gmail.com>

--- README.md
+++ README.md
@@ -1,28 +1,29 @@
 TDD 
 
-Describe: addressBook() 
+Describe: Destination() 
 
 test: "It should create destination object type with multiple properties"
 code: madrid; 
 expected output: 
 {location: "madrid", landmarks: ["La Sagrada Familia", "Gran Via", "Plaza Mayor"], time of year: "Fall"}
 
-test:
-code:
-expected output:
 
-test:
-code:
-expected output:
+Describe: addDestination()
 
-test:
-code:
-expected output:
+test: "It should assign unique ID for each destination addition to an addressBook"
+code: addressBook.addDestination(madrid);
+expected output: addressBook {1: madrid}
 
-test:
-code:
-expected output:
+describe: AddressBook()
 
-test:
-code:
-expected output:
+test: "It should add destination to addressBook"
+code: addressBook[0];
+expected output: madrid
+
+
+describe: assignId()
+
+test: "It will increase the Id number in the addressBook when used" 
+code: assignId();
+expected output: currenId = 1
+
--- scripts.js
+++ scripts.js
@@ -1,5 +1,37 @@
+//Business logic for addressBook//
+
+function AddressBook() {
+  this.destinations = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.addDestination = function(destination) {
+  destination.id = this.assignId();
+  this.destinations[destination.id] = destination;
+};
+
+//Business logic for Destination//
 function Destination (location, landmarks, timeOfYear) {
   this.location = location;
   this.landmarks = landmarks;
   this.timeOfYear = timeOfYear;
 }
+
+//Destinations
+let travelBook = new AddressBook();
+let madrid = new Destination("Madrid", ["La Sagrada Familia", "Gran Via", "Plaza Mayor"], "fall");
+let moscow = new Destination("Moscow", ["kremlin", "red square", "vodka centra"], "winter");
+let sydney = new Destination("Sydney", ["Opera House", "Bondi Beach", "Tower Eye"], "summer");
+
+travelBook.addDestination(madrid);
+travelBook.addDestination(sydney);
+travelBook.addDestination(moscow);
+
+
+//User Interface logic
+
